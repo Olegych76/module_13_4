25c3fa750d8bc06e0f0b8c74b845cec704a6f719
Olegych76 <f-o-r-j-o-b@yandex.ru>
2024-11-26 19:54:09 +0300

    Update module_13_4.py

--- module_13_4.py
+++ module_13_4.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 import asyncio
 
-api = '7652856175:AAFYFgExWGGfmxydR8PtxS1QfQXh5txyduQ'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
